a70acf3cd2e9b2105d990eb93cd7c6ac1487361f
Darsh <darshjvora@gmail.com>
2024-11-30 15:50:12 +0530

    Added Binary Tree Traversal Algorith and it's tests

---
+++ DataStructureAndAlgorithm/Algorithm/BinaryTree/BinaryTreeTraversal.cs
@@ -0,0 +1,68 @@
+﻿using DataStructureAndAlgorithm.DataStructure.BinaryTreeModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAndAlgorithm.Algorithm.BinaryTree
+{
+    public static class BinaryTreeTraversal
+    {
+        public static IEnumerable<T> BreadthFirstEnumerator<T>(this BinaryTreeNode<T> root)
+        {
+            if (root == null)
+            {
+                throw new ArgumentNullException(nameof(root));
+            }
+
+            Queue<BinaryTreeNode<T>> queue = new();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                yield return current.Value;
+
+                if (current.Left != null)
+                {
+                    queue.Enqueue(current.Left);
+                }
+
+                if (current.Right != null)
+                {
+                    queue.Enqueue(current.Right);
+                }
+            }
+        }
+
+        public static IEnumerable<T> DepthFirstEnumerator<T>(this BinaryTreeNode<T> root)
+        {
+            if (root == null)
+            {
+                throw new ArgumentNullException(nameof(root));
+            }
+
+            Stack<BinaryTreeNode<T>> stack = new();
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+
+                yield return current.Value;
+
+                if (current.Right != null)
+                {
+                    stack.Push(current.Right);
+                }
+
+                if (current.Left != null)
+                {
+                    stack.Push(current.Left);
+                }
+            }
+        }
+    }
+}
---
+++ Test/DataStructureAndAlgorithm.Test/Algorithm/BinaryTree/BinaryTreeTraversalTest.cs
@@ -0,0 +1,75 @@
+﻿using DataStructureAndAlgorithm.Algorithm.BinaryTree;
+using DataStructureAndAlgorithm.DataStructure.BinaryTreeModel;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace DataStructureAndAlgorithm.Test.Algorithm.BinaryTree
+{
+    [TestFixture]
+    internal class BinaryTreeTraversalTest
+    {
+
+        // SetUp method to initialize the test environment
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Creates tree
+        /// </summary>
+        /// <returns>Root node of tree</returns>
+        public static BinaryTreeNode<int> CreateTree()
+        {
+            //      5
+            //    /   \
+            //   6     7
+            //  / \     \
+            // 8   9    10
+            var root = new BinaryTreeNode<int>(5);
+            root.Left = new BinaryTreeNode<int>(6);
+            root.Right = new BinaryTreeNode<int>(7);
+            root.Left.Left = new BinaryTreeNode<int>(8);
+            root.Left.Right = new BinaryTreeNode<int>(9);
+            root.Right.Right = new BinaryTreeNode<int>(10);
+
+            return root;
+        }
+
+        // Test: Depth first search on Binary tree
+        [Test]
+        public void TestDepthFirstTraversal()
+        {
+            var root = CreateTree();
+            var result = string.Join(',', root.DepthFirstEnumerator());
+            ClassicAssert.AreEqual(result, "5,6,8,9,7,10");
+        }
+
+        // Test: Breadth first search on Binary tree
+        [Test]
+        public void TestBreadthFirstTraversal()
+        {
+            var root = CreateTree();
+            var result = string.Join(',', root.BreadthFirstEnumerator());
+            ClassicAssert.AreEqual(result, "5,6,7,8,9,10");
+        }
+
+        // Test: Depth first search on Binary tree
+        [Test]
+        public void TestDepthFirstTraversal_OnlyRoot()
+        {
+            var root = new BinaryTreeNode<int>(5);
+            var result = string.Join(',', root.DepthFirstEnumerator());
+            ClassicAssert.AreEqual(result, "5");
+        }
+
+        // Test: Breadth first search on Binary tree
+        [Test]
+        public void TestBreadthFirstTraversal_OnlyRoot()
+        {
+            var root = new BinaryTreeNode<int>(5);
+            var result = string.Join(',', root.BreadthFirstEnumerator());
+            ClassicAssert.AreEqual(result, "5");
+        }
+    }
+}
