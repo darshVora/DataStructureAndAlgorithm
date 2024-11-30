55f43338885eac84a178ffb501e6f9e64fbd1199
Darsh <darshjvora@gmail.com>
2024-11-30 14:42:55 +0530

    Added Binary tree node

---
+++ DataStructureAndAlgorithm/DataStructure/BinaryTreeModel/BinaryTreeNode.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructureAndAlgorithm.DataStructure.BinaryTreeModel
+{
+    public class BinaryTreeNode<T>(T value, BinaryTreeNode<T> left = null, BinaryTreeNode<T> right = null)
+    {
+        public T Value { get; set; } = value;
+        public BinaryTreeNode<T> Left { get; set; } = left;
+        public BinaryTreeNode<T> Right { get; set; } = right;
+    }
+}
+ 
